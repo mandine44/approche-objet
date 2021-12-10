52547e4810462b44731534eb530c3465e862a9b4
Mandine44 <amandine.huret@gmail.com>
2021-12-10 14:46:46 +0100

    commit4

---
+++ src/utils/ConversionNombre.java
@@ -0,0 +1,13 @@
+package utils;
+
+public class ConversionNombre {
+
+	public static void main(String[] args) {
+		String chaine = "12";
+		int entier = Integer.parseInt(chaine);
+		int a =3;
+		int b=5;
+		System.out.println(Integer.max(a, b));
+	}
+
+}
