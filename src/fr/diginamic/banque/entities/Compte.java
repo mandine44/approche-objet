991432d26a00b8cb745050940d05e299605e7eb2
Mandine44 <amandine.huret@gmail.com>
2021-12-10 14:55:30 +0100

    commit5

---
+++ src/fr/diginamic/banque/entities/Compte.java
@@ -0,0 +1,12 @@
+package fr.diginamic.banque.entities;
+
+public class Compte {
+	private int numeroCompte;
+	private int soldeCompte;
+	
+	public Compte(int numero,int solde ) {
+		numeroCompte = numero;
+		soldeCompte = solde;
+	}
+
+}
---
+++ src/fr/diginamic/banque/entities/TestBanque.java
@@ -0,0 +1,11 @@
+package fr.diginamic.banque.entities;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		Compte compte1= new Compte(254685, 25);
+		System.out.println (compte1);
+
+	}
+
+}
