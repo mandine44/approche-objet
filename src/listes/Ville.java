18e7eb7ea1c48defd186c0c57c4edaedae8bbc8b
Mandine44 <amandine.huret@gmail.com>
2021-12-15 10:17:55 +0100

    commit19

--- src/listes/Ville.java
+++ src/listes/Ville.java
@@ -1,6 +1,6 @@
 package listes;
 
-public class Ville {
+public class Ville implements Comparable<Ville>{
  public String nom;
  public int nbHabitants;
  
@@ -10,6 +10,44 @@ public Ville(String nom, int nbHabitants) {
 	this.nom = nom;
 	this.nbHabitants = nbHabitants;
 }
- 
+
+
+public String getNom() {
+	return nom;
+}
+
+
+public void setNom(String nom) {
+	this.nom = nom;
+}
+
+
+public int getNbHabitants() {
+	return nbHabitants;
+}
+
+
+public void setNbHabitants(int nbHabitants) {
+	this.nbHabitants = nbHabitants;
+}
+
+
+@Override
+public int compareTo(Ville autre) {
+	//return (this.getNom().compareTo(autre.getNom()));
+	
+	if (this.getNbHabitants()>autre.getNbHabitants()) {
+		return 1;
+	}
+	
+	if (this.getNbHabitants()<autre.getNbHabitants()) {
+		return -1;
+	}
+	return 0;
+	
+}
+
+
+
  
 }
---
+++ src/tri/ComparatorHabitant.java
@@ -0,0 +1,24 @@
+package tri;
+
+import java.util.Comparator;
+
+import listes.Ville;
+
+public class ComparatorHabitant implements Comparator<Ville>{
+
+	@Override
+	public int compare(Ville o1, Ville o2) {
+		
+		if (o1.getNbHabitants()>o2.getNbHabitants()) {
+			return 1;
+		}
+		
+		if (o1.getNbHabitants()<o2.getNbHabitants()) {
+			return -1;
+		}
+		return 0;
+	}
+	
+	
+
+}
